7acaaa0f7f81f82beac8c1e0811acdeecf981dd9
suranjanmaity <suranjanmaity4@gmail.com>
2023-02-01 23:48:35 +0530

    stopwatch css and js added

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ let pause = document.querySelector("#pause");
 let resume = document.querySelector("#resume");
 let lap = document.querySelector("#lap");
 let reset = document.querySelector("#reset");
+let lapList = document.querySelector(".lap");
+lapList.style.display = "none";
 pause.style.display = "none";
 resume.style.display = "none";
 start.addEventListener("click",()=>{
@@ -28,11 +30,10 @@ start.addEventListener("click",()=>{
             min = 0;
         }
         time.innerHTML=`
-        <h2> 
         ${hr}:${min}:${sec}:${mils++}
-        </h2>
         `;
     },10);
+    lapList.style.display = "flex";
 });
 pause.addEventListener("click",()=>{
     pause.style.display="none";
@@ -57,11 +58,21 @@ resume.addEventListener("click",()=>{
             min = 0;
         }
         time.innerHTML=`
-        <h2> 
         ${hr}:${min}:${sec}:${mils++}
-        </h2>
         `;
     },10);
+});
+let ul  = document.querySelector("ol");
+let count = 0;
+lap.addEventListener("click", ()=>{
+    if (hr >=0 && min >= 0 && sec >= 0 && mils > 0 && count < 5){
+        ul.innerHTML +=`
+        <li>
+        ${hr}:${min}:${sec}:${mils}
+        </li>
+        `;
+        count++;
+    }
 });
 reset.addEventListener("click",()=>{
     start.style.display="block";
@@ -73,9 +84,16 @@ reset.addEventListener("click",()=>{
     mils=0;
     clearInterval(timeCount);
     time.innerHTML=`
-    <h2>
-    ${hr}:${min}:${sec}:${mils++}
-    </h2>
+    ${hr}:${min}:${sec}:${mils}
     `;
-
+    ul.innerHTML = ``;
+    count = 0;
+    lapList.style.display = 'none';
+});
+let title = document.querySelector("title");
+window.addEventListener("blur",()=>{
+    title.innerHTML = "Come Back :-("
+});
+window.addEventListener("focus", ()=>{
+title.innerHTML = "Stop Watch"
 });
